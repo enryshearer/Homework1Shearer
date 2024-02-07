20aadf53ad4308ecd8f553ea4a230b16a224711f
MISTUser <MISTUser@lab8L4JUY>
2024-02-06 20:40:21 -0500

    Add project files.

---
+++ Homework1Shearer/Pages/Search Results.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework1Shearer.Pages
+{
+    public class Search_ResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
